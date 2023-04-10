f5d70a24baab5bb31d3f184cb31a136ada52ae7f
Duan Ryan <duanyuluo@gmail.com>
2023-04-11 06:33:22 +0800

    Update ccc13s5.py

--- ccc13s5.py
+++ ccc13s5.py
@@ -1,10 +1,6 @@
 #encoding = utf-8
 """CCC '13 S5 - Factor Solitaire
 Canadian Computing Competition: 2013 Stage 1, Senior #5
-In the game of Factor Solitaire, you start with the number 1, and try to change it to some given target number n by repeatedly using the following
-operation. In each step, if c is your current number, you split it into two positive factors a, b of your choice such that c = a x b. You then add a to
-your current number c to get your new current number. Doing this costs you b points.
-You continue doing this until your current number is n, and you try to achieve this at the cost of a minimum total number of points.
 For example, here is one way to get to 15:
 • start with 1
 • change 1 to 1 + 1 = 2 - cost so far is 1
@@ -12,7 +8,6 @@ For example, here is one way to get to 15:
 • change 3 to 3 + 3 = 6 - cost so far is 1 + 2 + 1
 • change 6 to 6 + 6 = 12 - cost so far is 1 + 2 + 1 + 1
 • change 12 to 12 + 3 = 15 - done, total cost is 1 + 2 + 1 + 1 + 4 = 9.
-In fact, this is the minimum possible total cost to get 15. You want to compute the minimum total cost for other target end numbers.
 """
 
 import ccc
@@ -21,28 +16,20 @@ target_num = 2013
 ccc.start_ccc()
 
 # cache all factors of a number for speeding
-factors_pool = {}   
 
 def get_factors(n):
-    if n in factors_pool:
-        return factors_pool[n]
-    factors_pool[n] = list(factor for i in range(1, int (n**0.5) + 1) if n % i == 0 for factor in (i, n//i)) 
     return factors_pool[n]
 
 # cache all search results of one number
 # key is number, value is the min cost path of key
-# a XXXX number have tens factors. if a length of path is 10, the try times maybe too huge.
-# So cache every lowest cost path of any number to target_num, can save many searching.
 search_lowcost_pool = {}
 
-# from current integer, try every factor of current integer until equel target integer
 # try path is current try history
 # return the lowest cost search path
 def search_lowcost_path(from_num, to_num):
 
     if (from_num in search_lowcost_pool):
         return search_lowcost_pool[from_num]
-    
     sub_min_score = 500000
     sub_min_path = []
 
@@ -68,17 +55,13 @@ def search_lowcost_path(from_num, to_num):
             continue
 
         # try a sub path
-        else:    
             sub_path = search_lowcost_path(from_num + sub_num, to_num)
-            if (sub_path != None and len(sub_path) > 0):
-                sub_path_score = sum(list(zip(*sub_path))[1])   # can cache too from optimized
 
                 # if sub path is a lower path, then replace min_path
                 if (sub_score + sub_path_score < sub_min_score):
                     sub_min_score = sub_score + sub_path_score
                     sub_min_path = [(sub_num, sub_score)] + sub_path
 
-    # if the lowest cost path which from from_num to to_num, cache the lowest cost path of [from_num]
     if (len(sub_min_path) > 0):
         search_lowcost_pool[from_num] = sub_min_path
 
